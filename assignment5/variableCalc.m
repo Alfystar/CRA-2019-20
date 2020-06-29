fe719d2b22e6724d2acfc89a2805591fb2139272
alfystar <alfystar1701@gmail.com>
2020-06-29 18:22:47 +0200

    ass 5 firp

--- assignment5/variableCalc.m
+++ assignment5/variableCalc.m
@@ -2,11 +2,13 @@ clear variables
 clc
 
 % Variabili non calcolate da Mathematica che servono
+% Sin ref
 aD2=1;     % Amp d2
 wD2=0.1;   % Freq d2
 
-aD1=0.5;
-wD1=50;
+% Quad dist
+aD1=10;
+wD1=10;
 
 
 run("Matrix.m")
@@ -14,4 +16,16 @@ run("Matrix.m")
 cAll=eye(length(Alin));
 dAll=zeros(length(Alin),1);
 
-x0=[0;0;0;0];
+S = [0 0 0
+     0 0 wD2
+     0 -wD2 0];
+bd = zeros(3,1);
+cd = eye(3);
+
+dd = zeros(3,1);
+
+d0 = [aD1;0;aD2];
+
+x0=[0;1;0;1];
+
+% L(1,1)=1;
