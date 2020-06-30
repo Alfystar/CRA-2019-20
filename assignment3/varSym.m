93486bc78f91f4b24844f04388aa4752a14b6cd6
alfystar <alfystar1701@gmail.com>
2020-06-30 18:43:32 +0200

    ass 3 pendente 5

---
+++ assignment3/varSym.m
@@ -0,0 +1,25 @@
+clear variable
+clc
+
+% Paramentri y1
+a1 = 1;
+w1 = 100;
+% Paramentri y2
+a2 = 1;
+w2 = 1;
+% Paramentri d
+ad1 =0.1;
+wd1 = 50;
+% Random d;
+md2 = 0;
+vd2 = 0.01;
+
+% Sin change
+wMax = 1;
+wChange=0.01;
+
+% Paramentri Stimatore I&I
+k1=1;
+
+est0 = [0;0];
+
