96c5c9f811e738c0c8af99af973f6cf966eefa11
alfystar <alfystar1701@gmail.com>
2020-07-02 10:54:53 +0200

    vista col prof

--- assignment5/variableCalc.m
+++ assignment5/variableCalc.m
@@ -4,12 +4,11 @@ clc
 % Variabili non calcolate da Mathematica che servono
 % Sin ref
 aD2=1;     % Amp d2
-wD2=0.1;   % Freq d2
+wD2=1;   % Freq d2
 
 % Quad dist
 aD1=10;
 wD1=10;
-
 
 run("Matrix.m")
 
@@ -20,12 +19,11 @@ S = [0 0 0
      0 0 wD2
      0 -wD2 0];
 bd = zeros(3,1);
-cd = eye(3);
-
-dd = zeros(3,1);
+cd = [0 1 0; 0 0 1];   % Solo stato sinusoidale
+dd = [0;0];
 
 d0 = [aD1;0;aD2];
 
 x0=[0;1;0;1];
 
-% L(1,1)=1;
+time = wD2^-1*2*pi*10; % Tempo parametrico di Simulazione
