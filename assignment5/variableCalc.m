a97896cfa7a627247216a05a23f0c49657494e51
alfystar <alfystar1701@gmail.com>
2020-07-17 17:08:11 +0200

    ass5 end

--- assignment5/variableCalc.m
+++ assignment5/variableCalc.m
@@ -3,15 +3,15 @@ clc
 
 % Variabili non calcolate da Mathematica che servono
 % Sin ref
-aD2=1;     % Amp d2
-wD2=1;   % Freq d2
+aD2=2;     % Amp d2
+wD2=2;   % Freq d2
 
 % Quad dist
-aD1=10;
-wD1=10;
+aD1=1;
+wD1=1;
 
 % Parametri delle matrici
-l=1; M=1;F=1;g=9.81;lam=-1;
+l=1; M=1;F=1;g=9.81;lam=-2;
 run("Matrix.m")
 % Definizione Sistema Esogeno
 S = [0 0 0
@@ -22,12 +22,12 @@ cd = [0 1 0; 0 0 1];   % Solo stato sinusoidale
 dd = [0;0];
 
 % Calcolo Matrici Controll Observer
-p=[-10 -10 -5 -5 -3 -3 -4];
-G = place(Ae',Ce',p)';
-G1=-G([1:4],:); G2=-G([5:7],:);
-F = [Alin+G1*Clin+Blin*K , Ptile+G1*Qe+Blin*L
+p=[-3 -3 -5 -5 -20 -4 -4];
+GOss = place(Ae',Ce',p)';
+G1=-GOss([1:4],:); G2=-GOss([5:7],:);
+FOss = [Alin+G1*Clin+Blin*K , Ptile+G1*Qe+Blin*L
     G2*Clin              ,     S+G2*Qe];
-H = [K L];
+HOss = [K L];
 Doss = [0 0];
 
 % Calcolo Matrici per FIRP
@@ -37,6 +37,7 @@ dAll=zeros(length(Alin),1);
 % Condizioni Iniziali
 d0 = [aD1;0;aD2];
 oss0 = zeros(7,1);
+% oss0(5) = aD1; % ampiezza iniziale del distrubi
 x0=[0;0;0;0];
 
 % Calcolo del tempo di Simulazione per K periodi
