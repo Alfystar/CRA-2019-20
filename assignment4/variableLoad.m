5b7a79f8dfaecc42c67895d5cd3ba9255b748961
alfystar <alfystar1701@gmail.com>
2020-07-19 17:53:32 +0200

    ass4 MRAC end

--- assignment4/variableLoad.m
+++ assignment4/variableLoad.m
@@ -1,13 +1,22 @@
 clear variable
 clc
 
-% Sistem Param
-a = 1; b = 2;
+% Sistem Params
+a = 1; a_m = -1;
 
-a_m = 0.7;
+[AWm,BWm,CWm ,DWm] = tf2ss(1,[1 -a_m]);
+[A,B,C ,D] = tf2ss(1,[1 -a]);
 
+x0Wm = 10;
+x0Gs = 10;
+
+
+% Variazioni di a
 a_ampl = 0.9;
 a_varFreq = 10; %2*pi/(24*3600);
 
 % Error size
 eMax = 0.4;
+
+% Estimation param
+gamma = 1;
